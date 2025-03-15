f5488c927fac9a94f11818d6df5124a0e450d439
manthan sahonta <manthansahonta1999@gmail.com>
2025-03-15 18:44:58 +0530

    HTML+ CSS +JS

---
+++ cooking.js
@@ -0,0 +1,57 @@
+fetch('https://dummyjson.com/recipes?limit=10&skip=10&select=name,image')
+    .then((data) => { return (data.json()) })
+    .then((DataRead) => {
+        console.log(DataRead)
+
+        let parentDiv = document.createElement('div');
+        parentDiv.className = 'div-container';
+        for (let i of DataRead.recipes) {
+            console.log(i);
+
+            let names = document.createElement('h4')
+            names.innerText = i.name;
+            document.body.appendChild(names);
+
+
+            let images = document.createElement('img');
+            images.className = 'photo';
+            images.src = i.image;
+            images.style.height = "200px";
+            images.style.width = "200px";
+            document.body.appendChild(images);
+
+
+            let divs = document.createElement('div');
+            divs.className = 'whole-api';
+
+            divs.appendChild(images);
+            divs.appendChild(names);
+            document.body.appendChild(divs);
+
+            parentDiv.appendChild(divs);
+
+
+
+        }
+
+        let htmldiv = document.querySelector(".div-container1");
+        htmldiv.appendChild(parentDiv);
+
+    })
+    .catch((dataRejected) => { dataRejected });
+
+
+let a = document.querySelector(".log");
+
+a.addEventListener("click", (e) => {
+    e.preventDefault()
+    for(let i of divCont){
+        i.style.display = "none"
+    }
+    newlog.style.display = "block"
+})
+    
+
+
+
+
